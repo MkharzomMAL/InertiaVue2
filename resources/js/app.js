c8a5659c97cf0731eeb6f0f50605227e21228781
Mkharzom <76581486+Mkharzom@users.noreply.github.com>
2023-05-04 14:39:31 +0800

    succsee

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-require('./bootstrap');
+import Vue from 'vue'
+import { createInertiaApp } from '@inertiajs/vue2'
+
+createInertiaApp({
+    resolve: name => {
+      const pages = require.context('./Pages', true, /\.vue$/i)
+      return pages(`./${name}.vue`)
+    },
+    setup({ el, App, props, plugin }) {
+      Vue.use(plugin)
+  
+      new Vue({
+        render: h => h(App, props),
+      }).$mount(el)
+    },
+  });
+  
